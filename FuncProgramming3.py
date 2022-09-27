8f90fca46c9464d9f9c44341ee4a16381405a133
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 19:32:12 +0300

    сделал крутую фишку с хвостами леммингов

--- FuncProgramming3.py
+++ FuncProgramming3.py
@@ -1,17 +1,34 @@
 #создаем леммингов
+# import random
+#
+# class Leming:
+#     total = 0
+#
+#     def __init__(self):
+#         обращаться через именование класса
+        # Leming.total += 1
+#
+# family = []
+# family_size = random.randint(16,32)
+#
+# while len(family) < family_size:
+#     new_leming = Leming()
+#     family.append(new_leming)
+# print(Leming.total)
 import random
-
-class Leming:
-    total = 0
+class leming:
+    names = ['peter', 'july', 'alex', 'max']
+    tail_lenght = 20
 
     def __init__(self):
-        #обращаться через именование класса
-        Leming.total += 1
+        self.tail_lenght = random.randint(15, 25)
+        self.name = random.choice(leming.names)
 
-family = []
-family_size = random.randint(16,32)
+    def __str__(self):
+        return 'name ' + self.name + ' tail lenght ' + str(self.tail_lenght)
 
-while len(family) < family_size:
-    new_leming = Leming()
-    family.append(new_leming)
-print(Leming.total)
+print(leming.tail_lenght)
+
+new_lemming = leming()
+print(new_lemming.tail_lenght)
+print(new_lemming)
