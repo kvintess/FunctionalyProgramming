c13cbfe2e8de723bbe26e50b6e5b93b55bedd701
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 18:24:16 +0300

    it is cool design!

--- FuncProgramming2.py
+++ FuncProgramming2.py
@@ -7,6 +7,13 @@ class Multiply:
         for item in args:
             list.append(item * self.factor)
         return list
-my_example = Multiply(factor=5)
-result = my_example(1,2,3,4,5)
-print(result)
+#my_example = Multiply(factor=5)
+#result = my_example(1,2,3,4,5)
+#print(result)
+
+list_of_multip_objects=[]
+for factor in (2,3,4,5):
+    mult = Multiply(factor=factor)
+    list_of_multip_objects.append(mult)
+for mul in list_of_multip_objects:
+    print(mul(10,20,30,40))
